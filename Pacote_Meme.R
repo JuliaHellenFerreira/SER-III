799f7c3e07603540c2f630d7779e12cb8e59b4f3
Julia Hellen Ferreira <38631847+JuliaHellenFerreira@users.noreply.github.com>
2019-02-13 23:05:05 -0200

    Script
    O pacote "Meme" permite usar uma imagem e anexar uma mensagem de texto. Com isso temos a possibilidade de criar memes e se divertir.
    
    Ps: Você pode usar este recurso não só para diversão. Caso queira  usar para uma trabalho acadêmico este pacote será muito útil

---
+++ Pacote_Meme.R
@@ -0,0 +1,23 @@
+#Criando memes
+
+install.packages("meme")
+
+require("meme")
+
+#Meme de "Cálculo"
+
+calculo<-"http://diariogaucho.rbsdirect.com.br/imagesrc/23020882.jpg?w=620"
+
+meme(calculo, "Quando eu lembro que terei", "que fazer Cálculo IIB")
+
+#Meme "Volta às aulas"
+
+aula <- "https://www.ocanal.com.br/wp-content/uploads/2019/01/wp-image-2068778646.jpg"
+
+meme(aula,"As aulas já", "vão voltar!")
+
+#Meme "Quero falar com você"
+
+falar<- "https://s2.glbimg.com/5MMAaRr7DCc18-7x8W4BH0IU37w=/640x424/top/i.glbimg.com/og/ig/infoglobo/f/original/2019/01/16/hana2.png"
+
+meme(falar, "Quero falar com você", "depois da aula,querida")
